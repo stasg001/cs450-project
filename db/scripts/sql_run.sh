a8f3583d218bb78eabeacd2000b8fd4421f1608f
Alex Launi <alex.launi+dev@gmail.com>
2021-02-20 18:20:08 -0500

    Fix bash syntax for running mysql via ssh from script
    Update sql_run.sh
    Update initial migration sql
    Rename follow on sql scripts to prepend migration id

--- db/scripts/sql_run.sh
+++ db/scripts/sql_run.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # 0: Check DB status
 # 1: Query MySql for latest migration
@@ -7,13 +7,19 @@
 
 ################################################################################
 # script and sql calls
-###############################################################################
+################################################################################
+
+DB_HOST=$1
+DB_USER=$2
+DB_PASSWORD=$3
+DB_NAME=$4
 
 #checking mysql connection
 mysql_db_statuscheck(){
 	echo "`date` :Checking DB connectivity...";
 	echo "`date` :Trying to connect to the ODU MySQL Database..."
-	echo "exit" | mysql -h ${DB_HOST} -u ${DB_USER} -p ${DB_PASSWORD} ${DB_NAME} -e "SELECT 1"
+	TAIL="-e 'SELECT 1;'"
+	ssh -o StrictHostKeyChecking=no -i sshkey ${REMOTE_USER}@${REMOTE_HOST} mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} ${TAIL}
 	if [[ $? -eq 0 ]]
 	then
 		DB_STATUS="UP"
@@ -23,38 +29,49 @@ mysql_db_statuscheck(){
 		DB_STATUS="DOWN"
 		export DB_STATUS
 		echo "`date` :Status: DOWN . Not able to Connect."
-		echo "`date` :Not able to connect to database with Username:
-		"${user}" HostName: ""${host}" DB Port: "${port}"." SID: "${service}"."
+		echo "`date`:Not able to connect to database with Username:
+		"${DB_USER}" HostName: ""${DB_HOST}" " SID: "${DB_NAME}"."
 		echo "`date` :Exiting Script Run..."
 		exit 1
 	fi
 }
 
 # run mysql function
-runmtsqls() {
+runmysqls() {
 	echo "`date` :Checking DB and table status..."
 
 	mysql_db_statuscheck
 
 	echo "`date` :DB status check completed"
-	echo "`date` :Connecting To ${user}/******@${service}";
+	echo "`date` :Connecting To ${DB_USER}/******@${DB_NAME}";
 	if [[ $DB_STATUS == "UP" ]]
 	then
 		# latest_migration will be an int
-		latest_migration=`mysql -h ${DB_HOST} -u ${DB_USER} -p ${DB_PASSWORD} ${DB_NAME} -se SELECT MAX(version) FROM MIGRATIONS;`
-		if [[ `$latest_migration |cut -c 1-10` -eq 'ERROR 1146' ]] then
+		TAIL="-se 'SELECT MAX(version) FROM migrations;'"
+		latest_migration=`ssh -o StrictHostKeyChecking=no -i sshkey ${REMOTE_USER}@${REMOTE_HOST} mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} ${TAIL} 2>&1`
+		if [[ `echo "${latest_migration}" |cut -c 1-10` == "ERROR 1146" ]]; then
+			echo "`date` :Running initial migration"
 			latest_migration=0
 		fi
 
-		for file in `ls ./db/sql`; do
-			file_migration_no=`$file |cut -c1-3`
-		   	if [[ $latest_migration -gt $file_migration_no ]] then
-				echo "`date`:Executing file $file...";
-				echo "`date`:__________________________________________";
-				echo "`date`:SQL OUTPUT:";
-				echo "`date`:__________________________________________";
-				mysql -h ${DB_HOST} -u ${DB_USER} -p ${DB_PASSWORD} ${DB_NAME} < $file
-			fi  
+		for file in `ls ./sql`; do
+			file_migration_no=`echo "$file" |cut -c1-3`
+		   	if [[ $latest_migration -lt $file_migration_no ]]; then
+				echo "`date` :Executing migration $file_migration_no from file $file...";
+				echo "`date` :__________________________________________";
+				echo "`date` :SQL OUTPUT:";
+				echo "`date` :__________________________________________";
+				TAIL="-se '`cat ./sql/${file}`'"
+				sqlout=`ssh -o StrictHostKeyChecking=no -i sshkey ${REMOTE_USER}@${REMOTE_HOST} mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} ${TAIL} 2>&1`
+				if [[ $? -eq 0 ]]; then 
+					TAIL="-se 'INSERT INTO migrations (version) VALUES (${file_migration_no});'"
+					ssh -o StrictHostKeyChecking=no -i sshkey ${REMOTE_USER}@${REMOTE_HOST} mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} ${TAIL}
+				else
+					echo ${sqlout}
+					exit 1
+				fi
+			fi
+		done
 	else
 		echo "`date` :Either the DB is down or the exit status returned by
 		the script shows ERROR."
