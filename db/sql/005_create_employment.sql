98b24b9873eedfdc376d26a1fa91e445e0e5d748
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:35:30 -0400

    troubleshooting gh actions fk issue

--- db/sql/005_create_employment.sql
+++ db/sql/005_create_employment.sql
@@ -9,6 +9,4 @@ CREATE TABLE IF NOT EXISTS tbl_fact_employment(
        ,workload smallint UNSIGNED
        ,start_date date
        ,end_date date
-       constraint fk_faclt_id foreign key (faculty_id)
-          references tbl_fact_users(id)
 );
--- db/sql/009_create_map_users_grants.sql
+++ db/sql/009_create_map_users_grants.sql
@@ -1,8 +1,3 @@
 CREATE TABLE IF NOT EXISTS tbl_map_grant_users(
             id smallint UNSIGNED NOT NULL auto_increment PRIMARY KEY
-           ,grant_id smallint UNSIGNED
-           constraint fk_grant_id foreign key (id)
-             references tbl_fact_users (id)
-           constraint fk_grant_id foreign key (grant_id)
-               references tbl_fact_grants (grant_number)
-);
+           ,grant_id smallint UNSIGNED);
