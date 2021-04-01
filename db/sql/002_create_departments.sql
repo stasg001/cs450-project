533b2ea2a2af3646496b8e41d2e09f1c05d5f56a
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:00:25 -0400

    added foreign keys to sql files

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
             id smallint UNSIGNED  NOT NULL auto_increment PRIMARY KEY
            ,dname varchar(30)
+           constraint fk_user_dpt foreign key (id)
+            references tbl_fact_users (id)
 );
--- db/sql/003_create_users.sql
+++ db/sql/003_create_users.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_users(
-       id int UNSIGNED AUTO_INCREMENT NOT NULL,
+       id int UNSIGNED AUTO_INCREMENT NOT NULL primary key,
        name varchar(30) NOT NULL,
        email varchar(15) NOT NULL unique, 
        password varchar(100) NOT NULL,
        created_date Timestamp,
        update_date TimeStamp,
        user_role ENUM('FACULTY', 'ADMINISTRATOR'),
-       department int UNSIGNED NOT NULL,
-       PRIMARY KEY(id)
+       department int UNSIGNED NOT NULL
 );
