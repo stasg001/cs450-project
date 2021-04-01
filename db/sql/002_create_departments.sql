64e29c025ad19bbfc50acab00f994c0b78d273c6
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:29:40 -0400

    continuing to t/s sql syntax error removed fk reference

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,6 +1,4 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
             id INT UNSIGNED  AUTO_INCREMENT NOT NULL PRIMARY KEY,
             dname varchar(30) not null
-            constraint fk_dpt_id foreign key (id)
-            references tbl_fact_users (department)
 );
