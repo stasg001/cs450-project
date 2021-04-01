7d596dd4f29601009dd813d81a8e5fba201cd4d0
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:12:40 -0400

    troubleshooting sql syntax error within 002_create_departments

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
             id smallint UNSIGNED  auto_increment NOT NULL PRIMARY KEY
            ,dname varchar(30)
-           constraint fk_user_dpmt foreign key (id)
-            references tbl_fact_users (id)
+            constraint fk_dpt_id foreign key (id)
+             references tbl_fact_users (id)
 );
