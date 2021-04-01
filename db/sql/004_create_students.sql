48ac6dcb8826c88fa4c3778fb7f678dc4e5a64a1
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:32:45 -0400

    continuing to t/s sql syntax error removed fk students

--- db/sql/004_create_students.sql
+++ db/sql/004_create_students.sql
@@ -3,8 +3,4 @@ CREATE TABLE IF NOT EXISTS tbl_fact_student(
         sname varchar(50),
         department smallint UNSIGNED,
         program ENUM('UG', 'MS', 'PHD')
-        constraint fk_stud_id foreign key (uin)
-           references tbl_fact_employment (student_uin)
-        constraint fk_stud_dpt foreign key (department)
-           references tbl_fact_departments (department)
 );
