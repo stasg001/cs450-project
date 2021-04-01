51d8ecc2d9278b3c21f3930006fe24656f3ba5d6
Alex Launi <dev@launi.me>
2021-04-01 16:32:13 -0400

    used wrong table names

--- db/sql/009_create_map_users_grants.sql
+++ db/sql/009_create_map_users_grants.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tbl_map_grant_users(
     grant_id int UNSIGNED,
     user_id int UNSIGNED,
     FOREIGN KEY (grant_id)
-        REFERENCES grants(id),
+        REFERENCES tbl_fact_grants(id),
     FOREIGN KEY (user_id)
-        REFERENCES users(id)
+        REFERENCES tbl_fact_users(id)
 );
