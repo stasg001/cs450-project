a92a5018a96490445d00427fbbe997dba2a5e74e
Alex Launi <dev@launi.me>
2021-04-01 16:34:23 -0400

    no real reason to use small int as an id on something of which there may potentially be many of

--- db/sql/008_create_grants.sql
+++ db/sql/008_create_grants.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS tbl_fact_grants(
-        id smallint UNSIGNED NOT NULL auto_increment PRIMARY KEY
-       ,grant_number varchar(20)
-       ,title varchar(30)
-       ,source_id smallint UNSIGNED
-       ,original_amt float(7,0)
-       ,balance float(7,2)
-       ,active ENUM('PENDING', 'APPROVED', 'DENIED')
-       ,administrator smallint UNSIGNED
+CREATE TABLE IF NOT EXISTS      (
+        id int UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+        grant_number varchar(20),
+        title varchar(30),
+        source_id smallint UNSIGNED,
+        original_amt float(7,0),
+        balance float(7,2),
+        active ENUM('PENDING', 'APPROVED', 'DENIED'),
+        administrator smallint UNSIGNED
 );
