f78d0e0dc245c739747403405e973b92355ca1ed
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:23:16 -0400

    changed is to int versus smallint

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
-            id smallint UNSIGNED  auto_increment NOT NULL PRIMARY KEY
+            id INT UNSIGNED  AUTO_INCREMENT NOT NULL PRIMARY KEY
            ,dname varchar(30)
             constraint fk_dpt_id foreign key (id)
              references tbl_fact_users (department)
