858a31cf7d7460c423644e2e9005119e540a32b9
Alex Launi <dev@launi.me>
2021-04-01 17:01:38 -0400

    Add foreign key reference on students table to departments

--- db/sql/004_create_students.sql
+++ db/sql/004_create_students.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_student(
         uin smallint UNSIGNED NOT NULL auto_increment PRIMARY KEY,
-        sname varchar(50),
-        department smallint UNSIGNED,
-        program ENUM('UG', 'MS', 'PHD')
+        name varchar(50),
+        department_id int UNSIGNED,
+        program ENUM('UG', 'MS', 'PHD'),
+        FOREIGN KEY (department)
+                REFERENCES tbl_fact_departments(id)
 );
