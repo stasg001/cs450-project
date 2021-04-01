14e4d57b919532490ac8562ce876cde4d1028283
Alex Launi <dev@launi.me>
2021-04-01 16:41:21 -0400

    Move commas to end of line in grants table script

--- db/sql/007_create_grants_entity.sql
+++ db/sql/007_create_grants_entity.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_granting_entity(
-            id smallint UNSIGNED NOT NULL auto_increment PRIMARY KEY
-           ,gname varchar(50)
-           ,gtype ENUM('EXTERNAL', 'INTERNAL')
+            id smallint UNSIGNED NOT NULL auto_increment PRIMARY KEY,
+            name varchar(50),
+            type ENUM('EXTERNAL', 'INTERNAL')
 );
