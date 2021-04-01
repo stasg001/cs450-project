08873c5a6762b9a2046e0542e30995baa28a3ce5
Alex Launi <dev@launi.me>
2021-04-01 16:45:29 -0400

    Updates to grants table creation
     * change precision on `original_amt`
     * change column name `active` to `status`
     * change column name `administrator` to `administrator_id`
     * Add foreign key references on `administrator_id` and `source_id`

--- db/sql/008_create_grants.sql
+++ db/sql/008_create_grants.sql
@@ -3,8 +3,12 @@ CREATE TABLE IF NOT EXISTS tbl_fact_grants(
         grant_number varchar(20),
         title varchar(30),
         source_id smallint UNSIGNED,
-        original_amt float(7,0),
+        original_amt float(7,2),
         balance float(7,2),
-        active ENUM('PENDING', 'APPROVED', 'DENIED'),
-        administrator smallint UNSIGNED
+        status ENUM('PENDING', 'APPROVED', 'DENIED'),
+        administrator_id smallint UNSIGNED,
+        FOREIGN KEY (source_id)
+                REFERENCES tbl_fact_granting_entity(id),
+        FOREIGN KEY (administrator_id)
+                REFERENCES tbl_fact_users(id)
 );
