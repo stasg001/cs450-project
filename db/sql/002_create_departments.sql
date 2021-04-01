09283c7734797c5f9bd705b770b3707b9f375343
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:05:25 -0400

    fixed type-o in pk on tbl_fact_users file

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
-            id smallint UNSIGNED  NOT NULL auto_increment PRIMARY KEY
+            id smallint UNSIGNED  auto_increment NOT NULL PRIMARY KEY
            ,dname varchar(30)
-           constraint fk_user_dpt foreign key (id)
+           constraint fk_user_dpmt foreign key (id)
             references tbl_fact_users (id)
 );
