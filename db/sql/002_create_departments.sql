8ece344f760d76b5a4b2999e241ad7b65bbd328f
Alex Launi <dev@launi.me>
2021-04-01 17:01:15 -0400

    rename dname column to just name

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
             id INT UNSIGNED  AUTO_INCREMENT NOT NULL PRIMARY KEY,
-            dname varchar(30) not null
+            name varchar(30) not null
 );
