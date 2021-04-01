aa98b6fc8f5eac61f2bc7e2c0e763a3411ff1f8e
jrich8573 <jasonrich85@icloud.com>
2021-03-31 21:25:25 -0400

    continuing to t/s sql syntax error

--- db/sql/002_create_departments.sql
+++ db/sql/002_create_departments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_fact_departments(
             id INT UNSIGNED  AUTO_INCREMENT NOT NULL PRIMARY KEY
            ,dname varchar(30)
-            constraint fk_dpt_id foreign key (id)
-             references tbl_fact_users (department)
+            --constraint fk_dpt_id foreign key (id)
+             --references tbl_fact_users (department)
 );
