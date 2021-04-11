84741314e1d4774bed0439bb3f820897fcc7a6f3
Alex Launi <dev@launi.me>
2021-04-11 16:41:25 -0400

    add default data for users, students, employments

---
+++ db/sql/013_add_default_users.sql
@@ -0,0 +1,6 @@
+iNSERT INTO tbl_fact_users (name, email, password, created_date, updated_date, user_role, department) VALUES
+        ('Alex Launi', 'alex@example.com', '$2y$10$MTfXNXdlax9zpiyWAh0.7uUSKZX81VfWOAFks7PhFAiahndV9Jpq.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1)
+        ('Josh Farr', 'josh@example.com', '$2y$10$tmJbAFwDx/meIsWpDxVcp.j/hNXtDZQBtnYv5ZuPYm1pGr3EwftNG', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1)
+        ('Jason Rich', 'jason@example.com', '$2y$10$lASdMGCqqFAw5giMAy1AN.QcK5MlJbQ./R2YZfKBnUFTcqk.mHkse', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1)
+        ('Suraksha Tasgaonkar', 'suraksha@example.com', '$2y$10$gEkssYEC4UfW6WB0W9UD1uM8b7lunVnxS8IW2dAGhCOeYMMjov0wy', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1)
+        ('Hot Dog', 'nothotdog@example.com', '$2y$10$g4jtfSNDTRoSN6dExddjP.vtg1LWoNJPgSr2JuRR9Hpgkf1HQ5w3q', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ADMINISTRATOR', 1);
---
+++ db/sql/014_add_students.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_fact_students (name, department_id, program)
+VALUES
+    ('Albert Einstein', 5, 'UG'),
+    ('Ada Lovelace', 2, 'PHD'),
+    ('Özlem Türeci', 3, 'PHD'),
+    ('Amanda Huggenkiss', 9, 'MS'),
+    ('Lâm Văn Túc', 7, 'UG');
