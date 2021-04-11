275351bbc6a0c0c25a1939544922be50813313f4
Alex Launi <dev@launi.me>
2021-04-11 16:54:04 -0400

    Add granting entities

---
+++ db/sql/016_add_grant_entities.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_fact_granting_entity (name, type)
+VALUES 
+    ('NASA', 'EXTERNAL'),
+    ('Barry Community Foundation', 'EXTERNAL'),
+    ('State of Wyoming', 'EXTERNAL'),
+    ('State of Idaho', 'EXTERNAL'),
+    ('State of Iowa', 'EXTERNAL');
