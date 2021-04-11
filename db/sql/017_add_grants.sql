f3b0569596310db8f21be31bd101a9fc8958b6c2
Alex Launi <dev@launi.me>
2021-04-11 17:04:36 -0400

    Add sample grants

---
+++ db/sql/017_add_grants.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_fact_grants (grant_number, title, source_id, original_amt, balance, status, administrator_id)
+VALUES
+    ('NASA-01', 'Nevada NASA Space Grant Consortium Undergraduate Scholarships', 1, 3000.0, 3000.0, 'PENDING', 1),
+    ('BCF-01', 'Thornapple Area Enrichment Foundation (TAEF) Grant - Mini Grant', 2, 300.0, 300.0, 'PENDING', 2),
+    ('WY-01', 'Wyoming Wildlife and Natural Resource Trust - Quick Grants', 3, 2500.0, 2500.0, 'PENDING', 3),
+    ('ID-01', 'Idaho Humanities Council - Research Fellowship Grants', 4, 3500.0, 3500.0, 'PENDING', 5),
+    ('IO-01', 'Iowa Artist Fellowship Program', 5, 10000.0, 10000.0, 'PENDING', 4);
